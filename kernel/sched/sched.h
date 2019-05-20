5b7f5c47de2b3471af6e26a1145e0a229bbedc7d
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2019-05-20 16:36:55 -0700

    sched/walt: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I5d89acdde73f5379d68ebc8513d0bbeaac128f5d
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Jonathan Avila <avilaj@codeaurora.org>
    [render: minor fixups]
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -108,7 +108,6 @@ struct sched_cluster {
 	unsigned int max_possible_freq;
 	bool freq_init_done;
 	u64 aggr_grp_load;
-	u64 coloc_boost_load;
 };
 
 extern cpumask_t asym_cap_sibling_cpus;
@@ -2061,6 +2060,7 @@ struct sched_walt_cpu_load {
 	unsigned long prev_window_util;
 	unsigned long nl;
 	unsigned long pl;
+	bool rtgb_active;
 	u64 ws;
 };
 
@@ -2084,6 +2084,7 @@ static inline unsigned long cpu_util_cum(int cpu, int delta)
 u64 freq_policy_load(struct rq *rq);
 
 extern u64 walt_load_reported_window;
+extern bool rtgb_active;
 
 static inline unsigned long
 __cpu_util_freq_walt(int cpu, struct sched_walt_cpu_load *walt_load)
@@ -2116,6 +2117,7 @@ __cpu_util_freq_walt(int cpu, struct sched_walt_cpu_load *walt_load)
 		walt_load->nl = nl;
 		walt_load->pl = pl;
 		walt_load->ws = walt_load_reported_window;
+		walt_load->rtgb_active = rtgb_active;
 	}
 
 	return (util >= capacity) ? capacity : util;
@@ -2975,7 +2977,6 @@ static inline enum sched_boost_policy task_boost_policy(struct task_struct *p)
 	return policy;
 }
 
-extern void walt_map_freq_to_load(void);
 extern void walt_update_min_max_capacity(void);
 
 static inline bool is_min_capacity_cluster(struct sched_cluster *cluster)
@@ -3107,7 +3108,6 @@ static inline bool early_detection_notify(struct rq *rq, u64 wallclock)
 }
 
 static inline void note_task_waking(struct task_struct *p, u64 wallclock) { }
-static inline void walt_map_freq_to_load(void) { }
 static inline void walt_update_min_max_capacity(void) { }
 #endif	/* CONFIG_SCHED_WALT */
 
